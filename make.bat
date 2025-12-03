45661a413c96b0cef66bbb279e6454ca38655b3b
Filter <not.so.filter@hotmail.com>
2025-12-03 15:36:48 -0500

    switch case and save a byte on make script

--- make.bat
+++ make.bat
@@ -1,5 +1,5 @@
 @echo off
-IF EXIST sound\z80.bin move /Y sound\z80.bin sound\z80.prev.bin >NUL
+if exist sound\z80.bin move /y sound\z80.bin sound\z80.prev.bin >NUL
 tool\windows\sjasmplus --lst=sound\z80.lst sound\z80.asm --raw=sound\z80.bin
-IF EXIST s1built.bin move /Y s1built.bin s1built.prev.bin >NUL
-tool\windows\asm68k /p /o ae-,l. main.asm, s1built.bin, , main.lst > main.log
+if exist s1built.bin move /y s1built.bin s1built.prev.bin >NUL
+tool\windows\asm68k /p /o ae-,l. main.asm, s1built.bin, , main.lst >main.log
