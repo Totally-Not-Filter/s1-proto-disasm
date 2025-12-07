7d5bda8cea3038fdd6552996345076042213700f
Filter <not.so.filter@hotmail.com>
2025-12-07 12:37:45 -0500

    lives window plane labelling

--- leftovers/routines/Lives Window Plane.asm
+++ leftovers/routines/Lives Window Plane.asm
@@ -1,40 +1,41 @@
 ;sub_485C:
+Lives_Window_Plane:
 		moveq	#0,d0
 		move.b	(v_lives).w,d1
 		cmpi.b	#2,d1
-		blo.s	loc_4876
+		blo.s	.lower
 		move.b	d1,d0
 		subq.b	#1,d0
 		cmpi.b	#5,d0
-		blo.s	loc_4876
+		blo.s	.lower
 		move.b	#4,d0
 
-loc_4876:
+.lower:
 		lea	(vdp_data_port).l,a6
 		locVRAM window_plane+$CBE
 		move.l	#(ArtTile_Early_Lives_Icon+8<<12)<<16|(ArtTile_Early_Lives_Icon+1)+8<<12,d2
-		bsr.s	sub_489E
+		bsr.s	.drawplane
 		locVRAM window_plane+$D3E
 		move.l	#((ArtTile_Early_Lives_Icon+2)+8<<12)<<16|(ArtTile_Early_Lives_Icon+3)+8<<12,d2
 
-sub_489E:
+.drawplane:
 		moveq	#0,d3
 		moveq	#4-1,d1
 		sub.w	d0,d1
-		blo.s	loc_48AC
+		blo.s	.drawnext
 
-loc_48A6:
+.drawloop:
 		move.l	d3,(a6)
-		dbf	d1,loc_48A6
+		dbf	d1,.drawloop
 
-loc_48AC:
+.drawnext:
 		move.w	d0,d1
 		subq.w	#1,d1
-		blo.s	locret_48B8
+		blo.s	.return
 
-loc_48B2:
+.drawlives:
 		move.l	d2,(a6)
-		dbf	d1,loc_48B2
+		dbf	d1,.drawlives
 
-locret_48B8:
+.return:
 		rts
