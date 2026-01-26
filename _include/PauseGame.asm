919d93789ebf111e8ad765dcde5e739325a6a5b0
Filter <not.so.filter@hotmail.com>
2026-01-26 12:19:01 -0500

    adjustments to PauseGame

--- _include/PauseGame.asm
+++ _include/PauseGame.asm
@@ -1,42 +1,47 @@
 ; ---------------------------------------------------------------------------
+; Subroutine to pause the game
+; ---------------------------------------------------------------------------
+
+; ||||||||||||||| S U B R O U T I N E |||||||||||||||||||||||||||||||||||||||
 
 PauseGame:
 		nop
-		tst.b	(v_lives).w
-		beq.s	loc_1206
-		tst.w	(f_pause).w
-		bne.s	loc_11CC
-		btst	#bitStart,(v_jpadpress1).w
-		beq.s	locret_120C
+		tst.b	(v_lives).w	; do you have any lives left?
+		beq.s	Unpause		; if not, branch
+		tst.w	(f_pause).w	; is game already paused?
+		bne.s	Pause_StopGame	; if yes, branch
+		btst	#bitStart,(v_jpadpress1).w ; is Start button pressed?
+		beq.s	Pause_DoNothing	; if not, branch
 
-loc_11CC:
-		move.w	#-1,(f_pause).w
+Pause_StopGame:
+		move.w	#-1,(f_pause).w	; freeze time
 
-loc_11D2:
+Pause_Loop:
 		move.b	#id_VInt_10,(v_vint_routine).w
 		bsr.w	WaitForVInt
-		btst	#bitA,(v_jpadpress1).w
-		beq.s	loc_11EE
-		move.b	#id_Title,(v_gamemode).w
+		btst	#bitA,(v_jpadpress1).w ; is button A pressed?
+		beq.s	Pause_ChkBC	; if not, branch
+		move.b	#id_Title,(v_gamemode).w ; set game mode to 4 (title screen)
 		nop
-		bra.s	loc_1206
-; ---------------------------------------------------------------------------
+		bra.s	Unpause
+; ===========================================================================
 
-loc_11EE:
-		btst	#bitB,(v_jpadhold1).w
-		bne.s	loc_120E
-		btst	#bitC,(v_jpadpress1).w
-		bne.s	loc_120E
-		btst	#bitStart,(v_jpadpress1).w
-		beq.s	loc_11D2
+Pause_ChkBC:
+		btst	#bitB,(v_jpadhold1).w ; is button B pressed?
+		bne.s	Pause_SlowMo	; if yes, branch
+		btst	#bitC,(v_jpadpress1).w ; is button C pressed?
+		bne.s	Pause_SlowMo	; if yes, branch
+		btst	#bitStart,(v_jpadpress1).w ; is Start button pressed?
+		beq.s	Pause_Loop	; if not, branch
 
-loc_1206:
-		move.w	#0,(f_pause).w
+Unpause:
+		move.w	#0,(f_pause).w	; unpause the game
 
-locret_120C:
+Pause_DoNothing:
 		rts
-; ---------------------------------------------------------------------------
+; ===========================================================================
 
-loc_120E:
+Pause_SlowMo:
 		move.w	#1,(f_pause).w
 		rts
+; End of function PauseGame
