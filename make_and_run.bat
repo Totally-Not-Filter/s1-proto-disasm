752de9ccc55872be8dcf6a5234bc8926d4d8e1e7
Filter <not.so.filter@hotmail.com>
2026-05-17 19:35:52 -0400

    make_and_run.bat added
    Closes #6

---
+++ make_and_run.bat
@@ -0,0 +1,9 @@
+@echo off
+
+REM // Convenience batch script to build the ROM and open it in the default emulator if compilation succeeds.
+REM // Requires ".bin" files to be associated with an emulator.
+
+call make.bat
+if exist s1built.bin (
+    start "" s1built.bin
+)
